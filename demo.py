ff1c4a7f053c07aab6f3cf17cf6fb961cef3cbe9
AverageJoeWang <oliverwang@oliverwangs-iMac-Pro.local>
2020-05-25 02:01:25 +0800

    update

--- demo.py
+++ demo.py
@@ -1,37 +1,39 @@
-#导入库
+# 导入库
 from model.model import ChatBot
-import torch,warnings,argparse
+import torch, warnings, argparse
+
 warnings.filterwarnings("ignore")
 
-#选择哪个模型 ，是否使用gpu
+# 选择哪个模型 ，是否使用gpu
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', help='The path of your model file.', required=True, type=str)
 parser.add_argument('--device', help='Your program running environment, "cpu" or "cuda"', type=str, default='cpu')
 args = parser.parse_args()
 print(args)
-#主程序，打印log，同时对话
+# 主程序，打印log，同时对话
 if __name__ == "__main__":
     print('Loading the model...')
     chatBot = ChatBot(args.model, device=torch.device(args.device))
     print('Finished...')
 
-    allRandomChoose,showInfo = False, False
-    #在控制端显示的词语
+    allRandomChoose, showInfo = False, False
+    # 在控制端显示的词语
     while True:
         inputSeq = input("我: ")
-        if inputSeq=='_crazy_on_':
+        if inputSeq == '_crazy_on_':
             allRandomChoose = True
-            print('聊天机器人: ','成功开启疯狂模式...')
-        elif inputSeq=='_crazy_off_':
+            print('聊天机器人: ', '成功开启疯狂模式...')
+        elif inputSeq == '_crazy_off_':
             allRandomChoose = False
-            print('聊天机器人: ','成功关闭疯狂模式...')
-        elif inputSeq=='_showInfo_on_':
+            print('聊天机器人: ', '成功关闭疯狂模式...')
+        elif inputSeq == '_showInfo_on_':
             showInfo = True
-            print('聊天机器人: ','成功开启日志打印...')
-        elif inputSeq=='_showInfo_off_':
+            print('聊天机器人: ', '成功开启日志打印...')
+        elif inputSeq == '_showInfo_off_':
             showInfo = False
-            print('聊天机器人: ','成功关闭日志打印...')
+            print('聊天机器人: ', '成功关闭日志打印...')
         else:
-            outputSeq = chatBot.predictByBeamSearch(inputSeq, isRandomChoose=True, allRandomChoose=allRandomChoose, showInfo=showInfo)
-            print('聊天机器人: ',outputSeq)
+            outputSeq = chatBot.predictByBeamSearch(inputSeq, isRandomChoose=True, allRandomChoose=allRandomChoose,
+                                                    showInfo=showInfo)
+            print('聊天机器人: ', outputSeq)
         print()
--- train.py
+++ train.py
@@ -1,17 +1,17 @@
-
-#导入库
+# 导入库
 from model.model import *
 from model.pre_process import *
 import torch
-#读入数据
+
+# 读入数据
 dataClass = Corpus('./data/qingyun.tsv', maxSentenceWordsNum=25)
-#指定模型和一些超参
+# 指定模型和一些超参
 model = Seq2Seq(dataClass, featureSize=256, hiddenSize=256,
                 attnType='L', attnMethod='general',
                 encoderNumLayers=5, decoderNumLayers=3,
                 encoderBidirectional=True,
                 device=torch.device('cpu:0'))
-#训练
+# 训练
 model.train(batchSize=1024, epoch=200)
-#保存模型
+# 保存模型
 model.save('modelL-1024-200.pkl')
